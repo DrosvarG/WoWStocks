433394472e4cb6a5d57e185e90085799133a9f21
Kentigern Hartmann <kentigern.h@gmail.com>
2024-12-26 01:32:37 +0100

    Remove user's Blizzard API credentials from config file

--- wowstocks/wowstocks.py
+++ wowstocks/wowstocks.py
@@ -3,6 +3,16 @@ import httpx
 
 from . import configuration
 
+class ClientCred:
+    def __init__(self, id:str = "", secret:str = ""):
+        self.client_id = id
+        self.client_secret = secret
+    
+    def __eq__(self, other):
+        if not isinstance(other, ClientCred):
+            return NotImplemented
+        return self.client_id == other.client_id and self.client_secret == other.client_secret
+    
 class WoWStocks:
     def __init__(self):
         self._config = configuration.Configuration('wowstocks/config.ini')
