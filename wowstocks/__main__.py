0d75123e90b562a17fe46c5077c402e4db08d0e7
Kentigern Hartmann <kentigern.h@gmail.com>
2024-12-26 01:29:55 +0100

    Implement a command line user interface

--- wowstocks/__main__.py
+++ wowstocks/__main__.py
@@ -1,11 +1,44 @@
-from . import wowstocks 
-from . import configuration
+from . import wowstocks
+
 import sqlite3
 
+def show_help(args:list[str]):
+    if len(args) == 0:
+        print("Type \"help \'command\'\" for more information. List of commands:")
+        print("help - Show this help message")
+        print("credentials - Reset credentials")
+        print("exit - Terminate the program")
+        print("servers - Print a list of all WoW servers")
+
+        return
+    
+    match args[0]:
+        case "exit":
+            print("Terminate the program. Syntax: exit")
+        case "help":
+            print("Get help about a command. Syntax: help \'command\'")
+        case "credentials":
+            print("Reset your credentials. Syntax: credentials (client_id client_secret)")
+        case "servers":
+            print("List Blizzard servers. Syntax: servers")
+
 def main():
-    config = configuration.Configuration('wowstocks/config.ini')
-    app = wowstocks.WoWStocks(config)
-    app.Run()
+    app = wowstocks.WoWStocks()
+
+    while True:
+        user_command = input("WoWStocks> ").split()
+        
+        match user_command[0]:
+            case "exit":
+                return
+            case "help":
+                show_help(user_command[1:])
+            case "credentials":
+                app.ResetCredentials()
+            case _:
+                print("Unknown command.")
+
+    return
 
 # DataFetcher.DataFetcher(get_credentials('config.ini'), db_connect('databases/auctions.db'))
 
--- wowstocks/wowstocks.py
+++ wowstocks/wowstocks.py
@@ -1,8 +1,16 @@
+import getpass
+import httpx
+
 from . import configuration
 
 class WoWStocks:
-    def __init__(self, config:configuration.Configuration):
-        return
+    def __init__(self):
+        self._config = configuration.Configuration('wowstocks/config.ini')
+        self.ResetCredentials()
+        self._http_client = httpx.Client()
 
-    def Run(self):
+        return
+    
+    def ResetCredentials(self):
+        self._cred = ClientCred(input("Enter client ID: "), getpass.getpass("Enter client Secret: "))
         return
